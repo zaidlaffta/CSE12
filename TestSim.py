c75282a982ec4909c4f6b6d49d131c81c6f37db9
zaidlaffta <bscmsc70@yahoo.com>
2024-10-13 00:58:19 -0700

    Update TestSim.py

--- TestSim.py
+++ TestSim.py
@@ -147,18 +147,18 @@ class TestSim:
 
 def main():
     s = TestSim();
-    s.runTime(10);
+    s.runTime(100);
     s.loadTopo("long_line.topo");
     s.loadNoise("no_noise.txt");
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
 
-    s.runTime(20);
+    s.runTime(200);
     s.ping(1, 2, "Hello, World");
-    s.runTime(10);
+    s.runTime(100);
     s.ping(1, 3, "Hi!");
-    s.runTime(20);
+    s.runTime(200);
 
 if __name__ == '__main__':
     main()
